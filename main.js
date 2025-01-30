e71dbea51ce9f11bbec462fbf8eca570e46ec480
jacobArquiza <jacob.arquiza@gmail.com>
2025-01-30 00:54:03 -0800

    Updated CSS, added Clear button

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let generateButton = document.querySelector("#generate-strokes");
+let speakButton = document.querySelector("#speak-button");
+let clearButton = document.querySelector("#clear-button");
+
+clearButton.addEventListener('click', clear);
 
 let output = document.querySelector('#output');
 generateButton.addEventListener('click', function(event) {
@@ -10,6 +14,7 @@ let outputHolder = document.querySelector("#output-holder")
 
 let input = document.querySelector("#input")
 
+
 function checkChinese(char){
     const chineseRegex = /[\u4e00-\u9fff]/;
     if (chineseRegex.test(char)) {
@@ -19,16 +24,24 @@ function checkChinese(char){
     }
 }
 
+function clear(e){
+    e.preventDefault();
+    outputHolder.id = 'output-holder';
+    output.innerHTML = "";
+}
+
 function generate() {
     outputHolder.id = '';
     output.innerHTML = "";
     const strInput = input.value;
+    let hasChinese = false;
 
     for (let i = 0; i < strInput.length; i++) {
         const char = strInput[i];
         if (checkChinese(char)) {
             const id = `char${i}`;
             output.innerHTML += `<div class='gifObject' id='${id}'></div>`;
+            hasChinese = true;
         }
     }
 
@@ -37,8 +50,8 @@ function generate() {
         if (checkChinese(char)) {
             const id = `char${i}`;
             const gifObject = document.querySelector(`#${id}`);
-            const gifWidth = gifObject.offsetWidth*0.95;
-            const gifHeight = gifObject.offsetHeight*0.95;
+            const gifWidth = gifObject.offsetWidth*0.70;
+            const gifHeight = gifObject.offsetHeight*0.70;
 
             const writer = HanziWriter.create(id, char, {
                 width: gifWidth,
@@ -47,10 +60,14 @@ function generate() {
                 showCharacter: false,
                 delayBetweenStrokes: 150,
                 strokeAnimationSpeed: 3,
-                delayBetweenLoops: 5000
+                delayBetweenLoops: 5000,
+                padding:0
             });
 
             writer.loopCharacterAnimation();
         }
+    }
+    if(!hasChinese){
+        outputHolder.id = 'output-holder';
     }
 }
